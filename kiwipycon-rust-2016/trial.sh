f709cc9485949257cc097a85273b1c2be59fcf00
Dan Mack <mack@macktronics.com>
2017-07-26 13:06:57 -0500

    adapt to run with homebrew truncate and gnu-time for mac osx
    Signed-off-by: Dan Mack <mack@macktronics.com>

--- kiwipycon-rust-2016/trial.sh
+++ kiwipycon-rust-2016/trial.sh
@@ -6,10 +6,10 @@ numargs=$#
 cmd="ministat -s -w 50"
 for ((i=1 ; i <= numargs ; i++))
 do
-  truncate --size=0 trial.$i
+  truncate -s 0 trial.$i
   # 10 iterations.
   for ignored in $(seq 10); do
-    /usr/bin/time --quiet -f "%e" $1 2>> trial.$i
+    gtime -f "%e" $1 2>> trial.$i
   done
   cmd="$cmd trial.$i"
   shift
